61107f1216d8fc6b55cba31039a05c6b4c2d35c6
aTagg <christag392@gmail.com>
2020-05-27 23:21:39 -0500

    well i tried

--- script.js
+++ script.js
@@ -7,18 +7,22 @@
 // Determine if the choice is equal to the selected answer
 
 // Element Variable
-var startBtn = document.querySelector("#startBtn");
+var startBtn = document.querySelector("#start-btn");
+var playAgainBtn = document.querySelector("#play-btn");
+var highScoreBtn = document.querySelector("#highscore-btn");
 var landingEl = document.querySelector("#landing");
+var endingEl = document.querySelector("#ending");
 var questionEl = document.querySelector("#questions");
 var questionTextEl = document.querySelector("#q-text");
 var choicesEl = document.querySelector("#choices");
-
+var timerEl = document.querySelector("#timer");
 
 // Question Bank
-var questionBank = [
+var questions = 
+[
     {
-        question: "Commonly used data types DO NOT include",
-        choices : [
+        text: "Commonly used data types DO NOT include",
+        choices: [
             "Boolean",
             "Alerts",
             "Strings",
@@ -27,17 +31,17 @@ var questionBank = [
         correctAnswer: 1
     },
     {
-        question: "The condition in an if / else statement is enclosed within ______. ",
-        choices : [
-            "Square Brackets",
-            "Curly Brackets",
-            "Quotes",
-            "Paranthesis",
+        text: "The condition in an if / else statement is enclosed within ______. ",
+        choices: [
+        "Square Brackets",
+        "Curly Brackets",
+        "Quotes",
+        "Paranthesis",
         ],
         correctAnswer: 3
     },
     {
-        question: "Arrays in JavaScript can be used to store _______. ",
+        text: "Arrays in JavaScript can be used to store _______. ",
         choices: [
             "Numbers and Strings",
             "Booleans",
@@ -47,8 +51,8 @@ var questionBank = [
         correctAnswer: 3
     },
     {
-        question: "String values must be enclosed within ______ when being assigned to variables. ",
-        qnswers: [
+        text: "String values must be enclosed within ______ when being assigned to variables. ",
+        choices: [
             "Commas",
             "Quotes",
             "Paranthesis",
@@ -57,57 +61,82 @@ var questionBank = [
         correctAnswer: 1
     },
     {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers: [
+        text: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: [
             "JavaScript",
             "Terminal / Bash",
             "Console Log",
             "For loops",
         ],
-        correctAnswer: "c"
+        correctAnswer: 2
     },
 ];
 
 var cursor = 0;
 var score = 0;
-var timeLeft = 75;
-var interval = 0;
+var timeRemaining = 10;
+var interval;
 
 choicesEl.addEventListener("click", function(event) {
     var element = event.target;
-    if (element.getAttribute("class") === "item") {
-    var id = parseInt(element.getAttribute("data-id"));    
-    if (question.correctAnswer === id) {
-        console.log("CORRECT ANSWER");
-        score++;
-    } else {
-        console.log("WRONG ANSWER");
-    }
-    cursor++;
-    renderQuestionData();
+    var question = questions[cursor];
+    if (element.className === "item") {
+        console.log("Make sure this is item");
+        var id = parseInt(element.getAttribute("data-id"));  
+        if (question.correctAnswer === id) {
+            console.log("Correct");
+            score++;
+        } else {
+            console.log("Wrong");
+            timerEl.textContent = timeRemaining - 3;
+        }
+        cursor++;
+        console.log("Score: ", score);
+        renderQuestionData();
+
     }
 });
 
-startBtn.addEventListener("click", function(event) {
-    landingEl.style.display = "none";
-    questionEl.style.display = "flex";
-    questionTextEl.textContent = "1. " + question.text;
+function renderQuestionData() {
+    var question = questions[cursor];
+    choicesEl.innerHTML = "";   
+    questionTextEl.textContent = question.text;
+    
     question.choices.forEach(function(choice, index) {
         var choiceItem = document.createElement("div");
         choiceItem.setAttribute("class", "item");
         choiceItem.setAttribute("data-id", index);
-        choiceItem.textContent
+        choiceItem.textContent = choice;
+        choicesEl.appendChild(choiceItem);
     });
+}
+
+function initializeTimer() {
+    timeRemaining = parseInt(timerEl.getAttribute("data-time"));
+    interval = setInterval(function () {
+        timeRemaining--;
+        if (timeRemaining > 0) {
+            timerEl.textContent = timeRemaining;
+        } else {
+            console.log("Game Over");
+            landingEl.style.display = "none";
+            questionEl.style.display = "none";
+            endingEl.style.display = "flex";
+            clearInterval(interval);
+        }
+    }, 1000);
+};
+
+
+startBtn.addEventListener("click", function (event) {
+    landingEl.style.display = "none";
+    questionEl.style.display = "flex"; 
+    renderQuestionData();
+    initializeTimer();
 });
-    
 
-
-
-// var currentQuestion = 0;
-// var viewAns = 0;
-// var correctAnswers = 0;
-// var quizOver = false;
-// var userSelectedAnswer = [];
-// var c = 180;
-// var t;
-
+playAgainBtn.addEventListener("click", function(event) {
+    landingEl.style.display = "flex";
+    endingEl.style.display = "none";
+    clearInterval(interval);
+});
